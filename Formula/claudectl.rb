b9721d8e45a67a74fadd4f33a4513dbf9f063b77
Barada Sahu <54206396+mercurialsolo@users.noreply.github.com>
2026-04-13 10:26:16 +0530

    Update claudectl to v0.12.0

--- Formula/claudectl.rb
+++ Formula/claudectl.rb
@@ -7,12 +7,12 @@ class Claudectl < Formula
   on_macos do
     on_arm do
       url "https://github.com/mercurialsolo/claudectl/releases/download/v0.12.0/claudectl-v0.12.0-aarch64-apple-darwin.tar.gz"
-      sha256 "a7d3594c3580a3129fce12685979cae4ddad1144f3a8e1a4b560dc2a37149ea9"
+      sha256 "f4f700beb3a1d46b9c2056d2400ecdf6e65c0bd2f2c3a7eed7375880b1522733"
     end
 
     on_intel do
       url "https://github.com/mercurialsolo/claudectl/releases/download/v0.12.0/claudectl-v0.12.0-x86_64-apple-darwin.tar.gz"
-      sha256 "6e73999f03756340152589fb7785fef5ebe4daa93e042bc2a19f449681569534"
+      sha256 "6ff9c1db6093c4fb42e1412a51b7f9e578da9ed074039506543e64ed7913baa8"
     end
   end
 
